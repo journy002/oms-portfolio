bd9f945d804a20a39a1cb0f063fbd67a1dfdf012
Munseok Oh <munseokoh@Munseoks-MacBook-Pro.local>
2022-01-16 02:25:06 +0900

    add scroll function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 import '../scss/style.scss'
 
+const nav = document.querySelector('.navbar');
 
-console.log('hhhi');
+nav.addEventListener('click', (event) => {
+    const target = event.target;
+    const data = target.dataset.type;
+    scrollInto(data)
+    
+});
+
+function scrollInto(selector) {
+    const scrollTo = document.querySelector(selector)
+    scrollTo.scrollIntoView({ behavior: 'smooth' })
+}
